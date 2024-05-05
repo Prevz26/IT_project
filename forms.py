4d9e76e2bcfee90bc9c69e995d5f82158f27a1ed
Precious Ojogu <nkangprecious26@gmail.com>
2024-05-05 19:00:36 +0100

    IT project

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class Registration(FlaskForm):
+    first_name = StringField(label="FIRST NAME ", validators=[DataRequired(), Length(min=4, max=10)])
+    
+    last_name = StringField(label="LAST NAME", validators=[DataRequired(), Length(min=4, max=20)])
+    
+    reg_no = StringField(label="REG-NO", validators=[DataRequired(), Length(min=10, max=10)])
+    
+    
+    department = StringField(label="DEPARTMENT", validators=[DataRequired()])
+    
+    faculty = StringField(label="FACULTY", validators=[DataRequired()])
+    
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    
+    submit = SubmitField("Submit")
+    reset = SubmitField("Reset")
+    
+class Login(FlaskForm):
+    reg_no = StringField(label="Reg Number", validators=[DataRequired(), Length(min=10, max=10)])
+    
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
+    
+    
+
